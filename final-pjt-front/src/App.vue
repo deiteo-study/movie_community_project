d7a6fcfa1d630d944102b88fa886be78f22017a0
송지윤 <songjiyoon@songjiyun-ui-MacBookAir.local>
2023-05-17 21:40:01 +0900

    DESIGN : NAV bar design(CSS) 수정

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
     <nav>
-      
       <router-link to="/home">
-        <img src="./assets/logo.png" alt="logo" style="width:35px;" >
+        <img src="./assets/home.png" alt="home" style="width:35px;" >
       </router-link>
-      <input type="text" name="" id="">
-      <router-link to="/profile">Profile</router-link>
+      <input type="text" name="" id="serch"  placeholder="검색어를 입력해주세요 :)">
+        <!-- <img src="./assets/magnify.png" alt="serch" style="width:18px; height:18px" >  -->
+
+      <router-link to="/profile">
+        <img src="./assets/user.png" alt="profile" style="width:40px;" > 
+      </router-link>
     </nav>
     <hr>
     <router-view/>
@@ -40,7 +43,17 @@ nav a.router-link-exact-active {
 
 <style scoped>
 input {
-  width: 60%;
-  border-radius: 20px;
+  width: 50%;
+  height: 30px;
+  border-radius: 18px;
+  margin-left: auto;
+  margin-right: auto;
+  border: solid 1.5px rgb(108, 119, 127);
+  padding-left: 20px;
 }
+input::placeholder {
+    color: #cccccc;
+    text-align: center;
+}
+
 </style>
