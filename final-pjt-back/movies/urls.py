abafc4c7dbb0746f527c92b579e2efc0a006bf2f
이성민 <lrune2el@naver.com>
2023-05-17 10:45:36 +0900

    FEAT : create movies, accounts app

---
+++ final-pjt-back/movies/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # path('movies/', views.movie_list)
+]
--- final-pjt-back/server/settings.py
+++ final-pjt-back/server/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # apps
+    'movies',
+    'accounts',
+    
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
