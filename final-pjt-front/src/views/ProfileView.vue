21d12a49ebee6adfb78b9d79fcf3ef7e859346ea
송지윤 <songjiyoon@songjiyun-ui-MacBookAir.local>
2023-05-23 02:00:58 +0900

    DESIGN : User Profile 디자인 수정(미완)

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="profile">
     <div v-if='userexist'>
+      <br>
+      <h2 v-if='me'>My Profile</h2>
+      <h2 v-else>{{username}}님의 프로필</h2>
+      <p> 임시로 구조 짜놓은 디자인!</p>
+      <p> 댓글 단게 너무 많으면 더보기로 줄일지! 고민</p>
+      <div>
+        <img src="@/assets/user5.png" alt="home" style="width:170px; height:170px;" >
+        <span><p>followers: 00  | followings: 00</p></span>
+      </div>
+      <hr>
       
-      <h1 v-if='me'>내 프로필</h1>
-      <h1 v-else>{{username}}님의 프로필</h1>
-  
-      <div>
-        <b>{{username}}이 좋아요한 영화</b>
-        <p v-for="(lm,idx) in user.like_movies" :key='idx'>{{lm.title}}</p>
-      </div>
-      <div>
-        <b>{{username}}이 좋아요한 리뷰</b>
-        <p v-for="(lr,idx) in user.like_reviews" :key='idx'>{{lr.content}}</p>
-      </div>
-      <div>
-        <b>{{username}}이 작성한 리뷰</b>
-        <p v-for="(wr,idx) in user.write_reviews" :key='idx'>{{wr.content}}</p>
-      </div>
-      <div>
-        <b>{{username}}이 작성한 댓글</b>
-        <p v-for="(wc,idx) in user.write_comments" :key='idx'>{{wc.content}}</p>
-      </div>
+      <h5>☃︎{{username}}이 좋아한 영화</h5>
+        <div class="box1">
+          <p v-for="(lm,idx) in user.like_movies" :key='idx'>- {{lm.title}}</p>
+        </div>
+      
+      <h5>☃︎{{username}}이 좋아한 리뷰</h5>
+        <div class="box2">
+          <p v-for="(lr,idx) in user.like_reviews" :key='idx'>- {{lr.content}}</p>
+        </div>
+      
+        <h5>☃︎{{username}}이 작성한 리뷰</h5>
+          <div class="box1">
+            <p v-for="(wr,idx) in user.write_reviews" :key='idx'>- {{wr.content}}</p>
+        </div>
+      
+        <h5>☃︎{{username}}이 작성한 댓글</h5>
+          <div class="box2">
+            <p v-for="(wc,idx) in user.write_comments" :key='idx'>- {{wc.content}}</p>
+        </div>
  
 
     </div>
@@ -81,3 +91,42 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+h5 {
+  /* font-family: 'Dongle', sans-serif; */
+  font-family: 'Gaegu', cursive;
+  /* font-family: 'Hi Melody', cursive; */
+  /* font-family: 'Sunflower', sans-serif; */
+  text-align: left;
+  margin-left: 220px;
+  margin-top: 5px;
+}
+
+img {
+  margin-bottom: 10px;
+}
+hr {
+  width: 70%;
+  margin: auto;
+}
+.box1 {
+  width: 70%;
+  border: solid 1px rgb(172, 172, 201);
+  border-radius: 0.7rem;
+  margin: 10px auto 20px auto;
+  padding: 10px 20px;
+  text-align: left;
+  
+}
+
+.box2 {
+  width: 70%;
+  border: none;
+  border-radius: 0.7rem;
+  background-color: rgb(232, 239, 246);
+  margin: 10px auto 20px auto;
+  padding: 10px 20px;
+  text-align: left;
+}
+</style>
