bcf19bb2d6a46b7fc018de75d5458e002de8315b
이성민 <lrune2el@naver.com>
2023-05-25 23:19:05 +0900

    CHORE : movieCard 마우스 커서 반응 추가

--- final-pjt-front/src/components/movies/MovieItemView.vue
+++ final-pjt-front/src/components/movies/MovieItemView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flip">
+    <div class="flip" >
         <div class="card" @click="detail(movie.id)" >
             <!-- 카드 앞면 -->
             <div class="front">
@@ -8,9 +8,9 @@
                 <!-- </div> -->
             </div>
             <!-- 카드 뒷면 -->
-            <div class="card back img1" :style="{ backgroundImage : `url(${poster_url})`}" > 
+            <div class="back"> 
                 <br>
-                <p class="cardtitle">{{ movie.title }}</p>  
+                <b class="cardtitle">{{ movie.title }}</b>  
                 <p class="cardtext">{{ movie.overview }}</p> 
             </div>
         </div>
@@ -77,13 +77,6 @@ img {
 .card-body {
     /* display: flex; */
 }
-.back p{
-    color: #e9e1cc;
-    /* text-align: center; */
-    /* line-height: 300px;   */
-    position: relative;
-    }
-
 .cardtext{
     white-space:normal;
     margin: 10px;    
@@ -91,9 +84,8 @@ img {
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
-    -webkit-line-clamp: 6;
+    -webkit-line-clamp: 7;
     -webkit-box-orient: vertical;
-    color: rgb(238, 228, 213);
 }
 
 * {
@@ -105,19 +97,17 @@ img {
 }
 
 .flip { 
+    cursor: pointer;
   width: 200px;
   height: 350px;
   position: relative; 
   perspective: 1100px;
-  margin: 1rem 2rem;
-  margin-bottom: 0rem;
-
+  margin: 2rem;
 }
 
 .card {
   /* width: 100%; 
   height: 100%;  */
-  margin-bottom: 0;
   width: 200px;
   height: 300px;
   position: relative;
@@ -136,30 +126,16 @@ img {
   color: #1f1f21;
 }
 
-.back { 
-  /* background: rgb(38, 39, 42, 0.8);  */
-  transform: rotateY(180deg);
-  background-size: cover;
-/* opacity: 0.5; */
-padding-bottom: 20px;
+.front {
+  /* background: tomato;  */
 }
-.back::before{
-        content: "";
-        opacity: 0.8;
-        position: absolute;
-        top: 0px;
-        left: 0px;
-        right: 0px;
-        bottom: 0px;
-        background-color: #383737;
-    }
+
+.back { 
+  /* background: rgb(38, 39, 42);  */
+  transform: rotateY(180deg);
+}
 
 .flip:hover .card {
   transform: rotateY(180deg);
 }
-.img2 {
-    /* background: rgba(142, 143, 146, 0.8);  */
-}
-
-
 </style>
